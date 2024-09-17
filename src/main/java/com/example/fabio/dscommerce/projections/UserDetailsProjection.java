0d6a20ba5edae94b49105abd3758c88fb9d82e5f
Fabio Morais <fabio.lmorais99@gmail.com>
2024-09-17 15:59:59 -0300

    Feat: UserDetailsService

---
+++ src/main/java/com/example/fabio/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.example.fabio.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
